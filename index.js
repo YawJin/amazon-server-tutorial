48565a4bc09094485b898ffe75d4707d23b8d4f4
Yaw Jin Ching <yawjinching@Yaws-MacBook-Pro.local>
2022-10-04 12:09:49 +0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,39 @@
+// IMPORTS FROM PACKAGES
+const express = require("express");
+const mongoose = require("mongoose");
+const adminRouter = require("./routes/admin");
+// IMPORTS FROM OTHER FILES
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+
+// INIT
+const PORT = process.env.PORT || 3000;
+const app = express();
+// const DB =
+//   "mongodb+srv://rivaan:rivaan123@cluster0.wpyhk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const DB = "mongodb+srv://yawjin:7ddYE3fiwgxBDUGl@cluster0.53iumfk.mongodb.net/test?retryWrites=true&w=majority"
+// const DB = "mongodb://root1:iNWq16wzzRzJZ1HZZEY0fqXZ@localhost:27017/test?authSource=admin";
+
+
+// middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+// Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+// app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
+  console.log(`connected at port ${PORT}`);
+});
